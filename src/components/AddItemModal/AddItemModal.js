15e77cf004e43d30d82e10a705dab2b2a0538698
Jerry W Jackson <jerry.w.jackson@gmail.com>
2023-12-14 09:16:53 -0600

    removed unneeded AddIten<odal.css file

--- src/components/AddItemModal/AddItemModal.js
+++ src/components/AddItemModal/AddItemModal.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
-import './AddItemModal.css'
 
 const AddItemModal = ({handleCloseModal, onAddItem, isOpen}) => {
   const [name, setName] = useState('');
