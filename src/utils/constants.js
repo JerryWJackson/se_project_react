ef033981dc5a65ee93bda2eda0865c793e184489
Jerry W Jackson <jerry.w.jackson@gmail.com>
2024-03-28 14:10:29 -0500

    remove superflous items from constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -68,8 +68,7 @@ const units = "Imperial";
 
 export const endpointUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
 
-export const baseUrl = "http://localhost:3001/items";
-export const BASE_URL = "http://localhost:3000/";
+export const baseUrl = "http://localhost:3001/";
 export const defaultHeaders = {
   Accept: "application/json",
   "Content-Type": "application/json",
