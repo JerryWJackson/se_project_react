b5cf9f310e1ed12e033ac2812e0dc22de9a97eec
Jerry W Jackson <jerry.w.jackson@gmail.com>
2024-06-27 20:42:31 -0500

    added comments to token.js showing that calling handleToken without params would delete any tokens in local storage

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,14 +1,26 @@
 const TOKEN_KEY = "jwt";
 
+/**
+ * getToken
+ * returns token if one is present in local storage.
+ *
+ * @returns token
+ */
+
 export const getToken = () => {
   return localStorage.getItem(TOKEN_KEY);
 };
 
+/**
+ * handleToken
+ * ! if called without passing any params, handleToken will remove the
+ * ! entire jwt token array from local storage.
+ *
+ * @param {*} token
+ * @returns token value, if any
+ */
 export const handleToken = (token) => {
   if (token) {
-    console.log(
-      "got token, if not already present it will be added to local storage"
-    );
     localStorage.setItem(TOKEN_KEY, token);
     return token;
   } else {
@@ -16,6 +28,15 @@ export const handleToken = (token) => {
     return;
   }
 };
+
+/**
+ * checkToken
+ * this is a helper function to check if a token matches any tokens
+ * within the jwt token array held in local storage.
+ *
+ * @param {*} tokenToCheck
+ * @returns boolean
+ */
 
 export const checkToken = (tokenToCheck) => {
   const token = getToken();
