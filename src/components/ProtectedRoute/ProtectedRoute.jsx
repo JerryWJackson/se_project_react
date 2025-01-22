bb777bf1c43f130fa5420fbacd7134c80ae923f2
Jerry W Jackson <jerry.w.jackson@gmail.com>
2025-01-22 16:31:16 -0600

    Fix ProtectedRoute component breaking on edge case

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,9 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children, isLoggedIn }) {
-  if (!isLoggedIn) return null;
-  console.log("You are logged in, loading your profile.");
-  return isLoggedIn ? children : <Navigate to="/" />;
+  isLoggedIn ? children : <Navigate to="/" />;
 }
 
 export default ProtectedRoute;
