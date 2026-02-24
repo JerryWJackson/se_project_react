fb283b35c0df576157c1f246654f673ee2676b0c
Jerry W Jackson <jerry.w.jackson@gmail.com>
2026-02-24 01:52:03 -0600

    Finalize design and deployment: fix routing basename, add homepage, and polish UI metrics

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,10 @@ import { ModalProvider } from "./contexts/ModalContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ModalProvider>
         <App />
       </ModalProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
